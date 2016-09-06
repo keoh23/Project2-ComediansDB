4186b6f3d75edab489c71b4eae3283113e3942f8
keoh23 <kevin.oh23@gmail.com>
2016-09-06 14:51:52 -0700

    created basic set up of databasehelper and making sure it works before adding more

---
+++ ComediansDatabase/app/src/main/java/com/example/owen/comediansdatabase/Comedians.java
@@ -0,0 +1,17 @@
+package com.example.owen.comediansdatabase;
+
+/**
+ * Created by Owen on 9/6/2016.
+ */
+public class Comedians {
+
+    int id;
+    String name;
+    String year;
+
+    public Comedians(int id, String name, String year) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+}
